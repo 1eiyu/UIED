9fa1b382a63561aceeaf05f3d5bd90ac0c7c3336
1eiyu <91090819+1eiyu@users.noreply.github.com>
2022-06-19 18:24:15 +0200

    Update Config.py

--- cnn/Config.py
+++ cnn/Config.py
@@ -4,7 +4,6 @@ class Config:
 
         self.DATA_PATH = "./training_data"
 
-        # self.MODEL_PATH = 'E:/Mulong/Model/rico_compos/cnn2-textview.h5'
         self.class_map = ['Button', 'Text', 'Noise']
 
         self.image_shape = (32, 32, 3)
