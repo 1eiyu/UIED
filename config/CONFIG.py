12782da3699ba4416c2dec390480ab178ed564cb
1eiyu <91090819+1eiyu@users.noreply.github.com>
2022-06-19 18:51:39 +0200

    Update CONFIG.py

--- config/CONFIG.py
+++ config/CONFIG.py
@@ -7,12 +7,12 @@ class Config:
     def __init__(self):
         # setting CNN (graphic elements) model
         self.image_shape = (32, 32, 3)
-        self.CNN_PATH = parent_path + '/cnn/weights/cnn-test.h5'
+        self.CNN_PATH = os.path.join(parent_path, 'cnn','weights','cnn-test.h5')
         self.element_class = ['Button', 'Text', 'Noise', 'Slidebar']
         self.class_number = len(self.element_class)
-        self.model_saved = parent_path + '/cnn/weights/cnn-test.h5'
+        self.model_saved = os.path.join(parent_path, 'cnn','weights','cnn-test.h5')
         # setting EAST (ocr) model
-        self.EAST_PATH = parent_path + '/east_icdar2015_resnet_v1_50_rbox'
+        self.EAST_PATH = os.path.join(parent_path, 'east_icdar2015_resnet_v1_50_rbox')
 
         self.COLOR = {'Button': (0, 255, 0),
                       'Text': (0, 0, 255),
